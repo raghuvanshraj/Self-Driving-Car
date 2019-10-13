ac614c67bcabcb8931ee8fb2217b03566c45ae15
raghuvanshraj <raghuvansh1997@gmail.com>
2019-10-13 14:22:12 +0530

    completed project

--- ai.py
+++ ai.py
@@ -1,14 +1,14 @@
 import random
 import numpy as np
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
-
-capacity = 100000
-temperature = 7
+import datetime
+import glob
 
 
 class Network(nn.Module):
@@ -41,22 +41,77 @@ class ReplayMemory(object):
 
     def sample(self, batch_size):
         samples = zip(*random.sample(self.memory, batch_size))
-        return map(lambda x: Variable(torch.cat(x, 0), samples))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
 
 
 class DQN(object):
 
     def __init__(self, input_size, nb_action, gamma):
+        self.capacity = 100000
+        self.temperature = 100
         self.gamma = gamma
         self.reward_window = []
         self.model = Network(input_size, nb_action)
-        self.memory = ReplayMemory(capacity)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2)
-        self.last_state = torch.FloatTensor().unsqueeze(0)
+        self.memory = ReplayMemory(self.capacity)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        self.last_state = torch.FloatTensor(input_size).unsqueeze(0)
         self.last_action = 0
         self.last_reward = 0
 
     def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile=True)) * temperature)
+        probs = F.softmax(self.model(Variable(state, volatile=True)) * self.temperature)
         action = probs.multinomial()
-        return action.data[0,0]
+        return action.data[0, 0]
+
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state)\
+            .gather(1, batch_action.unsqueeze(1))\
+            .squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma * next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_graph=True)
+        self.optimizer.step()
+
+    def update(self, reward, new_signal):
+        new_state = torch.FloatTensor(new_signal).unsqueeze(0)
+        self.memory.push((self.last_state, new_state, torch.FloatTensor([self.last_reward]),
+                          torch.LongTensor([int(self.last_action)]),
+                          ))
+        action = self.select_action(new_state)
+        if len(self.memory.memory) > 100:
+            batch_state, batch_next_state, batch_reward, batch_action = self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+
+        self.last_action = action
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+        if len(self.reward_window) > 1000:
+            del self.reward_window[0]
+
+        return action
+
+    def score(self):
+        return sum(self.reward_window) / (len(self.reward_window) + 1.)
+
+    def save(self):
+        torch.save({
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+        },
+            os.path.join('saved_brain', datetime.datetime.strftime(datetime.datetime.now(), '%M%S%f.pth'))
+        )
+
+    def load(self):
+        saved_brains = glob.glob(os.path.join('saved_brain', '*.pth'))
+        saved_brains.sort(reverse=True)
+        if len(saved_brains) == 0:
+            print('No model to load')
+        else:
+            print('Loading model ' + saved_brains[0])
+            checkpoint = torch.load(saved_brains[0])
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+
--- requirements/requirements.sh
+++ requirements/requirements.sh
@@ -5,7 +5,7 @@ echo;
 conda create -n Self-Driving-Car python=2.7
 echo "Activating Environment..."
 echo;
-source activate Self-Driving-Car
+conda activate Self-Driving-Car
 echo "Installing PyTorch 0.3.1..."
 echo;
 conda install pytorch==0.3.1 -c pytorch
